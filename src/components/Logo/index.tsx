fd30f6c5394ec439db4b7c5edad9786b34b3a611
adamr04 <adam.radwan@akg-wien.at>
2022-12-17 19:59:11 +0100

    alle dateien importiert

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Logo.styles.css";
+
+export const Logo: React.FC = () => {
+  return (
+    <div className="logo">
+      <em>43. Schulball</em>
+      <small>Akademisches Gymnasium Wien</small>
+    </div>
+  );
+};
